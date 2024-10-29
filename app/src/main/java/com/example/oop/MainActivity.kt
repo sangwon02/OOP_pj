7eac1f715a70691174f26e9215996c1929ef0e03
이상원 <sangwon020214@gmail.com>
2024-10-29 20:03:08 +0900

    내비게이션 바 완성 후 커밋

---
+++ app/src/main/java/com/example/oop/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.oop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.oop.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        binding.bottomNav.setupWithNavController(navController)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+
+
+
+    }
+}
