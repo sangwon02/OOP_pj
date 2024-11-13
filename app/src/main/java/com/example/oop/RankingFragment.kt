bde51138a36eae0ebc9389459f3cf6b8425e1d4c
박경훈 <baggyeonghun@baggyeonghun-ui-MacBookAir.local>
2024-11-13 14:50:08 +0900

    학생들 이름 중 하나가 반복되어 조금 보기좋게 바꿨습니다.

--- app/src/main/java/com/example/oop/RankingFragment.kt
+++ app/src/main/java/com/example/oop/RankingFragment.kt
@@ -12,7 +12,7 @@ import com.example.oop.databinding.FragmentRankingBinding
 import com.example.oop.viewmodel.UserAdapter
 
 class RankingFragment : Fragment() {
-    val rankingList = arrayOf(
+    val rankingList = arrayOf(      // 리더보드 페이지의 사용자 포함 친구(사용자 객체 User.kt) 리스트입니다.
         User("박경훈"),
         User("이상원"),
         User("곽도혁"),
@@ -24,17 +24,23 @@ class RankingFragment : Fragment() {
         User("문경현")
     )
 
+    // fragment_ranking.xml의 뷰 요소를 코드에서 쉽게 다루게 합니다. 뷰 바인딩을 사용합니다.
     lateinit var binding : FragmentRankingBinding
 
+    // Fragment가 호출될 때 호출되는 초기화 메소드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    // Fragment의 UI를 생성하는 메소드
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        // XML 레이아웃을 inflate(xml코드들을 객체화), binding 객체 초기화
         binding = FragmentRankingBinding.inflate(inflater, container, false)
 
         // Fragment 클래스 내부에서 this는 context로 간주되지 않아 requireContext()를 사용해야 한다.
+        // recUsers:
         binding.recUsers.layoutManager = LinearLayoutManager(requireContext())
         binding.recUsers.adapter = UserAdapter(rankingList)
 
--- app/src/main/java/com/example/oop/data/User.kt
+++ app/src/main/java/com/example/oop/data/User.kt
@@ -1,3 +1,4 @@
 package com.example.oop.data
 
+// 추후 프로필 이미지, 리더보드 순위, 금일 달성률을 멤버로 가져야합니다.
 data class User (val name: String)
